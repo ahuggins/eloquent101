a0d3ee1b760b41e34eb7da27085e4e5c2453fbb8
Andrew huggins <andrewhuggins@gmail.com>
2017-11-25 18:19:22 -0500

    add stub for new posts

---
+++ resources/views/blankPost.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.site')
+
+@section('heading', 'Getting Started')
+
+@section('youtube', 'Youtube_id_here')
+
+@section('content')
+@markdown
+# Add Markdown content here.
+@endmarkdown
+@stop
