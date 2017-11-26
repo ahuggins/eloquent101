4948cab9bfa4f787a91c9d86e9c9788d0de4fd24
Andrew huggins <andrewhuggins@gmail.com>
2017-11-25 22:41:56 -0500

    update Youtube link on home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,6 @@ You can download this site and use the code/db to learn on your own, simple clon
 
 A little about me, my name is Andy, I've been using Laravel for four, almost five, years and I have learned a lot about it in that time. Eloquent is one of the pieces that I enjoy quite a bit, but I still see many questions on forums about how to do things with Eloquent. My goal with this course is start as if you have never used it before, then build on top of that knowledge and get into some more advanced parts of Eloquent.
 
-If you find this site useful, consider following me on Twitter [@andy_huggins](https://twitter.com/andy_huggins) and be sure to subscribe to my channel on Youtube [](https://youtube.com/AndyHuggins) so you can be notified when each lesson comes out!
+If you find this site useful, consider following me on Twitter [@andy_huggins](https://twitter.com/andy_huggins) and be sure to subscribe to [my channel](https://youtube.com/AndyHuggins) on Youtube so you can be notified when each lesson comes out!
 @endmarkdown
 @stop
