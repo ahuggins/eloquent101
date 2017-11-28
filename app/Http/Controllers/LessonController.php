f11d4ffca14d2858c1842f55fe24745f10469da4
Andrew huggins <andrewhuggins@gmail.com>
2017-11-28 01:18:00 -0500

    rename ExamplesController to LessonController

---
+++ app/Http/Controllers/LessonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Lesson\LessonFactory;
+use App\Services\LessonService;
+
+class LessonController extends Controller
+{
+    protected $lesson;
+
+    function __construct(LessonService $lesson)
+    {
+        $this->lesson = $lesson;
+    }
+
+    public function show($lesson, $exhibit)
+    {
+        return $this->lesson->example($lesson, $exhibit);
+    }
+}
