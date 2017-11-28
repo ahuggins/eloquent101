503051612919ace77f6710d2cdef873f0d328681
Andrew huggins <andrewhuggins@gmail.com>
2017-11-28 01:37:11 -0500

    rename lesson view to example

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -18,25 +18,25 @@ Total Execution Time: {{ $sql->sum('time') }}
     Values: {{ implode(',', $query['bindings']) }}
     @endif
     Time: {{ $query['time'] }} 
-
 @endforeach
 ```
 
 ## Returned Data
 {{ dump($data) }}
+
 @endmarkdown
 @stop
 
 @section('scripts')
-<script type="text/javascript">
-(function() {
-    'use strict';
-var compacted = document.querySelectorAll('.sf-dump-compact');
+    <script type="text/javascript">
+        (function() {
+            'use strict';
+        var compacted = document.querySelectorAll('.sf-dump-compact');
 
-for (var i = 0; i < compacted.length; i++) {
-  compacted[i].className = 'sf-dump-expanded';
-}
+        for (var i = 0; i < compacted.length; i++) {
+          compacted[i].className = 'sf-dump-expanded';
+        }
 
-})();
-</script>
+        })();
+    </script>
 @stop
